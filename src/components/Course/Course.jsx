25a10b3419433dee7314341c06890d2b0a44c479
junaeddurjoy <junaed.durjoy@gmail.com>
2023-09-15 07:26:08 +0600

    credit calculation bar added

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 
 const Course = ({ course }) => {
-    const { title, cover, description } = course;
+    const { title, cover, description, price, credit } = course;
     return (
         <div className="card  bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
+            <figure className="px-2 pt-2">
                 <img src={cover} alt="Shoes" className="rounded-xl" />
             </figure>
-            <div className="card-body items-center text-center">
-                <h2 className="card-title font-semibold text-xl pt-2">{title}</h2>
-                <p className='text-gray-500 pb-3'>{description}</p>
-                
+            <div className="card-body items-center">
+                <h2 className="card-title text-xl font-semibold text-center pt-2">{title}</h2>
+                <p className='text-gray-500 pb-3 text-justify'>{description}</p>
+                <div className='font-semibold flex justify-evenly'>
+                    <p className='text-gray-500 pb-3 text-justify'>Price: {price}</p>
+                    <p className='text-gray-500 pb-3 text-justify'>Credit: {credit}hr</p>
+                </div>
                 <div className="card-actions">
                     <button className="btn btn-primary bg-blue-600 hover:bg-blue-400 text-white p-2 rounded-xl w-full">Select</button>
                 </div>
--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -1,17 +1,18 @@
 const Credits = () => {
   return (
-    <div>
-      <div className="card w-64 bg-base-100 shadow-xl -mr-24">
-        <h3 className="text-xl font-bold text-blue-500">Credit Hour Remaining: </h3>
+    <div className="w-96 px-3 py-3">
+      <div className="bg-base-100 shadow-xl -mr-24 text-left p-5">
+        <h3 className="text-lg font-bold text-blue-500">Credit Hour Remaining: 20hr</h3>
         <hr />
-        <h3 className="text-xl font-bold">Course Name </h3>
-        <ol>
-
+        <h3 className="text-lg font-bold">Course Name </h3>
+        <ol className="">
+          <li>Introduction to C++</li>
+          <li>Database Design</li>
         </ol>
         <hr />
-        <p>Total Credit Hour: </p>
+        <p>Total Credit Hour: 14hr</p>
         <hr />
-        <p className="font-bold">Total Price: </p>
+        <p className="font-bold">Total Price: 345000tk</p>
       </div>
     </div>
   )
